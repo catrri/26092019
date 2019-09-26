28ae66487fb468bdac566bb91bb7b6422b4fd384
LeonidLS <lscomp@ukr.net>
2019-09-26 20:36:06 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,3 +2,8 @@
 TESbbb мое не трогать! bla bla bla
 2cac2ac3daabaf0d961e4bbc492a937168d713d7fssfddsf
 fsdsfsfddssfdsfsdfsfsfdsdf ffffffffffffffffffffffffffd88888888888888888888888888888888888888888888
+
+
+
+ git config --global user.email "you@example.com"
+ git config --global user.name "Your Name"
