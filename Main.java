77e640b6f7849bf316c7ccc3d852d220c3bb8c8b
catrri <catrri@gmail.com>
2019-09-27 10:35:03 +0300

    new change

--- Main.java
+++ Main.java
@@ -1,12 +1,8 @@
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhh
+hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhеуые
 TESbbb мое не трогать! bla bla bla
 2cac2ac3daabaf0d961e4bbc492a937168d713d7fssfddsf
 fsdsfsfddssfdsfsdfsfsfdsdf ffffffffffffffffffffffffffd88888888888888888888888888888888888888888888
 
 
 
- git config --global user.email "you@example.com"
- git config --global user.name "Your Name"
 
- 
- git remote add origin https://github.com/LeonidLS/26092019.git
