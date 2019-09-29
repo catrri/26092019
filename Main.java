b83547aa16734d832c03996b3b5ff1d2e7473d74
LeonidLS <llsuschenko@gmail.com>
2019-09-29 20:08:44 +0300

    new_izmenenie

--- Main.java
+++ Main.java
@@ -1,5 +1,5 @@
-hhhhhhhhhhhhhhhhizmenenie-pervoy-stroki
-TESbbb мое не трогать! bla bla bla
+﻿hhhhhhhhhhhhhhhhizmenenie-pervoy-stroki
+Эти изменения могут привести к конфликту.
 2cac2ac3daabaf0d961e4bbc492a937168d713d7fssfddsf
 fsdsfsfddssfdsfsdfsfsfdsdf ffffffffffffffffffffffffffd88888888888888888888888888888888888888888888
 
