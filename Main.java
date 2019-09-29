ef177f9c96539e09c0b38a1fa84a922abfb86b79
LeonidLS <llsuschenko@gmail.com>
2019-09-29 20:01:01 +0300

    izmenenie

--- Main.java
+++ Main.java
@@ -1,4 +1,4 @@
-hhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhhеуые
+hhhhhhhhhhhhhhhhizmenenie-pervoy-stroki
 TESbbb мое не трогать! bla bla bla
 2cac2ac3daabaf0d961e4bbc492a937168d713d7fssfddsf
 fsdsfsfddssfdsfsdfsfsfdsdf ffffffffffffffffffffffffffd88888888888888888888888888888888888888888888
